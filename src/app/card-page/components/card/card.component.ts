7bd38090bb5b394ec0dad24448cd0e31e159b14e
Ion <ioncazacu13@gmail.com>
2022-08-16 13:54:23 +0300

    Added TvGrid Buttons

--- src/app/card-page/components/card/card.component.ts
+++ src/app/card-page/components/card/card.component.ts
@@ -1,17 +1,40 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild, HostBinding,Directive } from '@angular/core';
 import { IAbonnementGrid } from '../../models/abonnement';
 import { IAbonnement } from '../../models/card-index';
+
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent{
   @Input() abonnement: IAbonnement;
-  @ViewChild('tvPlusGrid') tvPlusGrid: ElementRef;
-  @ViewChild('tvProGrid') tvProGrid: ElementRef;
-
+  @Input() abonnementGrid: IAbonnementGrid;
+  @ViewChild('channels') channels: ElementRef;
+  @ViewChild('price') price: ElementRef;
+  @ViewChild('promoPrice') promoPrice: ElementRef;
+  @ViewChild('progressBar') progress: ElementRef;
+  @ViewChild('tvPlusBtn') tvPlus: ElementRef;
+  @ViewChild('tvProBtn') tvPro: ElementRef;
   selectedGridprice: IAbonnementGrid;
-  
+
+  tvPlusGrid(){
+    this.channels.nativeElement.innerHTML = this.abonnement.grids[0].channelsSd + " / " + this.abonnement.grids[0].channelsHd + "HD";
+    this.price.nativeElement.innerHTML = this.abonnement.grids[0].prices.price + " lei lunar";
+    this.promoPrice.nativeElement.innerHTML = this.abonnement.grids[0].prices.promotionalPrice + " lei lunar";
+    this.progress.nativeElement.style.width = this.abonnement.grids[0].progressPercentage + "%";
+    this.tvPlus.nativeElement.classList.add('active');
+    this.tvPro.nativeElement.classList.remove('active');
+  }
+
+  tvProGrid(){
+    this.channels.nativeElement.innerHTML = this.abonnement.grids[1].channelsSd + " / " + this.abonnement.grids[1].channelsHd + "HD";
+    this.price.nativeElement.innerHTML = this.abonnement.grids[1].prices.price + " lei lunar";
+    this.promoPrice.nativeElement.innerHTML = this.abonnement.grids[1].prices.promotionalPrice + " lei lunar";
+    this.progress.nativeElement.style.width = this.abonnement.grids[1].progressPercentage + "%";
+    this.tvPro.nativeElement.classList.add('active');
+    this.tvPlus.nativeElement.classList.remove('active');
+  }
+
 }
